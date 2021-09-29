e50dbfa09bf6b334da980a76194c67d423f54272
Payne81 <3334248237@qq.com>
2021-09-29 17:58:50 +0800

    fmt fix

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,46 +1,59 @@
 extern crate clap;
 use clap::{App, AppSettings, Arg, SubCommand};
-use std::process::exit;
 use kvs::KvStore;
+use std::process::exit;
 
 fn cliprocess(kvstore: &mut KvStore) {
-
     let matches = App::new(env!("CARGO_PKG_NAME"))
-                          .version(env!("CARGO_PKG_VERSION"))
-                          .author(env!("CARGO_PKG_AUTHORS"))
-                          .about(env!("CARGO_PKG_DESCRIPTION"))
-                          .setting(AppSettings::DisableHelpSubcommand)
-                          .setting(AppSettings::SubcommandRequiredElseHelp)
-                          .setting(AppSettings::VersionlessSubcommands)
-                          .subcommand(SubCommand::with_name("set")
-                                      .about("Set the value of a string key to a string")
-                                      .version("0.1")
-                                      .author("Payne Z. <3334248237@qq.com>")
-                                      .arg(Arg::with_name("KEY")
-                                           .help("Sets the string key")
-                                           .required(true)
-                                           .index(1))
-                                      .arg(Arg::with_name("VALUE")
-                                           .help("Sets the string Value")
-                                           .required(true)
-                                           .index(2)))
-                          .subcommand(SubCommand::with_name("get")
-                                      .about("Get the string value of a given string key")
-                                      .version("0.1")
-                                      .author("Payne Z. <3334248237@qq.com>")
-                                      .arg(Arg::with_name("KEY")
-                                           .help("Get the string key")
-                                           .required(true)
-                                           .index(1)))
-                          .subcommand(SubCommand::with_name("rm")
-                                      .about("Remove a given key")
-                                      .version("0.1")
-                                      .author("Payne Z. <3334248237@qq.com>")
-                                      .arg(Arg::with_name("KEY")
-                                           .help("Remove a given key")
-                                           .required(true)
-                                           .index(1)))    
-                          .get_matches();
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .version("0.1")
+                .author("Payne Z. <3334248237@qq.com>")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("Sets the string key")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("Sets the string Value")
+                        .required(true)
+                        .index(2),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get the string value of a given string key")
+                .version("0.1")
+                .author("Payne Z. <3334248237@qq.com>")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("Get the string key")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Remove a given key")
+                .version("0.1")
+                .author("Payne Z. <3334248237@qq.com>")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("Remove a given key")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .get_matches();
     if let Some(matches) = matches.subcommand_matches("set") {
         let key = matches.value_of("KEY").unwrap().to_owned();
         let val = matches.value_of("VALUE").unwrap().to_owned();
@@ -53,8 +66,8 @@ fn cliprocess(kvstore: &mut KvStore) {
         eprintln!("unimplemented");
         exit(1);
         match kvstore.get(key) {
-            Some(val)   => println!("{}", val),
-            None        => panic!(),
+            Some(val) => println!("{}", val),
+            None => panic!(),
         }
     }
     if let Some(matches) = matches.subcommand_matches("rm") {
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ pub struct KvStore {
 }
 
 impl KvStore {
-    pub fn new() -> KvStore{
-        let mut kv_db: HashMap<String, String> = HashMap::new();
-        KvStore { kv_db }
+    pub fn new() -> KvStore {
+        // let kv_db: HashMap<String, String> = HashMap::new();
+        KvStore { kv_db: HashMap::new() }
     }
 
     pub fn set(&mut self, key: String, value: String) {
@@ -18,13 +18,11 @@ impl KvStore {
 
     pub fn get(&self, key: String) -> Option<String> {
         // println!("KvStore get {}", key);
-        match self.kv_db.get(&key) {
-            Some(val) => Some(val.clone()),
-            _         => None,
-        }
+        // self.kv_db.get(&key).map(|val| val.clone())
+        self.kv_db.get(&key).cloned()
     }
 
-    pub fn remove(&mut self, key: String){
+    pub fn remove(&mut self, key: String) {
         // let val = self.get(key);
         self.kv_db.remove(&key);
     }
